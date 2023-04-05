717374af689a98c9834013aa2423fffa3075a585
no-ki <nolanit229@gmail.com>
2023-04-05 19:59:19 +0800

    update

--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -1,32 +1,33 @@
-import { OpenAIStream, OpenAIStreamPayload } from "../../utils/OpenAIStream";
+import { OpenAIStream, OpenAIStreamPayload } from '../../utils/OpenAIStream'
 
 type RequestData = {
-  messageText: string;
-};
-
+  messageText: string
+}
 
 if (!process.env.OPENAI_API_KEY) {
-  throw new Error("Missing env var from OpenAI");
+  throw new Error('Missing env var from OpenAI')
 }
 
 export const config = {
-  runtime: "edge",
-};
+  runtime: 'edge',
+}
 
-let message_junk = "";
+let message_junk = ''
 
 const handler = async (req: Request): Promise<Response> => {
-  
-  const { messageText } = (await req.json()) as RequestData;
+  const { messageText } = (await req.json()) as RequestData
 
-  message_junk += `${messageText} \n` ;
+  message_junk += `${messageText} \n`
 
   if (!messageText) {
-    return new Response("No prompt in the request, Please check README.md else please contact via my mail: zhenghu61919@gmail.com", { status: 400 });
+    return new Response(
+      'No prompt in the request, Please check README.md else please contact via my mail: ryancharley429@gmail.com',
+      { status: 400 }
+    )
   }
 
   const payload: OpenAIStreamPayload = {
-    model: "text-davinci-003",
+    model: 'text-davinci-003',
     prompt: message_junk,
     temperature: 0.7,
     top_p: 1,
@@ -35,10 +36,10 @@ const handler = async (req: Request): Promise<Response> => {
     max_tokens: 1000,
     stream: true,
     n: 1,
-  };
+  }
 
-  const stream = await OpenAIStream(payload);
-  return new Response(stream);
-};
+  const stream = await OpenAIStream(payload)
+  return new Response(stream)
+}
 
-export default handler;
+export default handler
